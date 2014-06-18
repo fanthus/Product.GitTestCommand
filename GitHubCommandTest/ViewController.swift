4cb1553916e85489fa9633b77b244bbc3c81b8e8
fanxiushan <fanxiushan@gmail.com>
2014-06-18 23:23:45 +0800

    First init
    Signed-off-by: fanxiushan <fanxiushan@gmail.com>

---
+++ GitHubCommandTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubCommandTest
+//
+//  Created by xiushan.fan on 18/6/14.
+//  Copyright (c) 2014年 moji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
